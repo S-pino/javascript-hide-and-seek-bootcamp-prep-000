f2c519b0752ce9880d88b7d6211ff58bb3d6a025
Stephanie Pino <stephy_pino@hotmail.com>
2020-10-02 00:29:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function getFirstSelector(selector) {
+return querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.getElementByID(nestedTarget);
+}
+
+function increaseRankBy(n) {
+ const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+ for (let i = 0; i < lis.length; i++) {
+   lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+ }
+}
